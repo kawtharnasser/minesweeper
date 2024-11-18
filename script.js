2c4f8d32ad7a763d67848079c8e64a0eaaa14b00
kawtharnasser <kawtherhabib.ko@gmail.com>
2024-11-18 18:50:15 +0300

    fixed errors

--- script.js
+++ script.js
@@ -1,35 +1,91 @@
 // Constants
 const emojis = {
   normal: '😊',
-  rightClick: '😮',
+  shock: '😮',
   win: '😎',
   lose: '😵',
   flag: '🚩',
   mine: '🦈'
 }
 
+const gameMine = {
+  original: '💣',
+  underwater: '🦈',
+  lava: '💥',
+  jurassic: '🦖'
+}
+const images = {
+  original: {
+    normal: '',
+    win: '',
+    lose: ''
+  },
+  underwater: {
+    normal:
+      "url('https://th.bing.com/th/id/R.a61d96f23882c8e5a47ab16a6a7d7207?rik=QGLtMMZ1AoMqYw&riu=http%3a%2f%2fwww.pixelstalk.net%2fwp-content%2fuploads%2f2016%2f06%2fOcean-underwater-light-wallpaper-hd.jpg&ehk=RLmSM8K8ZQQn3YDnpMQGZfb5E3dJHQENNRqudKJONlY%3d&risl=1&pid=ImgRaw&r=0')",
+    win: "url('')",
+    lose: "url('https://th.bing.com/th/id/R.75da500dcfd5b8dcd13d811667205f55?rik=0euedb0IWDkLOw&riu=http%3a%2f%2fmedia.giphy.com%2fmedia%2fNZymcDPuVrVZu%2fgiphy.gif&ehk=8xus2w%2f5xowTgSx2aDren6WW4fTh1EGFJHXsmBpMITY%3d&risl=&pid=ImgRaw&r=0')"
+  },
+  lava: {
+    normal: '',
+    win: '',
+    lose: ''
+  },
+  jurassic: {
+    normal: '',
+    win: '',
+    lose: ''
+  }
+}
+
 // Variables
-let board = [] // The game grid
+let board = [] // The game grid in js
 let revealedCount = 0 // Number of bombs revealed
+let gameStarted = false // For the startGame function
+let isGameOver = false // to stop players actions when lose
+let isWin = false
+let totalCells = 0 // to check for winning
+
 let timer = false // For the game timer
 let seconds = 0 // For the timer
-let gameStarted = false // For the startGame function
-let totalCells = 0 // The number of cells
-let audio = new Audio('Untitled video - Made with Clipchamp.mp4')
-let flagClick = new Audio('rightclick.mp4')
-// Function to initiate the game board
-let rows = 16 // Number of inner arrays
-let columns = 16 // Number of elements in each inner array
-let mines = 40
 
-let minesRemain = document.querySelector('#right-side-box').innerHTML
+// the initial number of rows, and col
+let rows = 4
+let columns = 4
+let mines = 1
+
+//sound Effects
+let loseAudio = new Audio('Untitled video - Made with Clipchamp.mp4')
+let flagClick = new Audio('rightclick.mp4')
+
+let winAudio = new Audio('winbrass-39632.mp3')
+
+//elements selector
+//the game levels
+const body = document.querySelector('#index') //for design
 const beginner = document.querySelector('#beginner')
 const intermediate = document.querySelector('#intermediate')
 const advance = document.querySelector('#advance')
 
-// Functions
-// Function to start the game stopwatch
+//game displays
+const minesRemain = document.querySelector('#right-side-box')
+const emojiButton = document.querySelector('#reset-button')
 const stopwatch = document.querySelector('#stopwatch')
+const messageBox = document.querySelector('#message')
+//the game grid
+const grid = document.querySelector('#grid')
+
+//for customization
+const original = document.getElementById('original')
+
+const gameDisplayers = document.querySelectorAll('.game-displayer')
+const gameMode = document.querySelector('#game-mode')
+// lava
+// underwater
+// jurassic
+
+// Functions
+//1) Function to start the game stopwatch
 function startTimer() {
   if (!timer) {
     timer = setInterval(() => {
@@ -39,47 +95,46 @@ function startTimer() {
   }
 }
 
+//2) a function that create the game board
 const initiateBoard = () => {
-  board = [] // Reset the board for a new game
+  board = [] // Reset the board each round
+
   for (let i = 0; i < rows; i++) {
-    let innerArray = []
+    let cellStatues = []
     for (let j = 0; j < columns; j++) {
-      innerArray.push({
+      cellStatues.push({
         revealed: false,
         flagged: false,
         mine: false,
         adjacentMines: 0
       })
     }
-    board.push(innerArray)
+    board.push(cellStatues)
   }
   totalCells = rows * columns - mines
   placeMines()
-  calculateAdjacents()
-  console.log(board)
+  calculateAdjacent()
 }
 
-// Function to generate mines
+//3) Function that add mines to the board
 const placeMines = () => {
   let placedMines = 0
   while (placedMines < mines) {
+    //to give the bomb a random location
     const row = Math.floor(Math.random() * rows)
     const col = Math.floor(Math.random() * columns)
     if (!board[row][col].mine) {
-      board[row][col].mine = true // Only set the mine property
+      board[row][col].mine = true
       placedMines++
     }
   }
 }
 
-// Function to calculate the number of mines surrounding each cell
-const calculateAdjacents = () => {
+//4) Function to calculate the number of mines surrounding each cell
+const calculateAdjacent = () => {
   for (let r = 0; r < board.length; r++) {
     for (let c = 0; c < board[r].length; c++) {
-      if (board[r][c].mine) {
-        continue // Skip mine cells
-      }
-      let count = 0
+      let count = 0 //for each cell
       for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
           const newRow = r + i
@@ -92,24 +147,23 @@ const calculateAdjacents = () => {
             newCol < columns &&
             board[newRow][newCol].mine
           ) {
-            count++ // Increment count for each adjacent mine
+            count++ //increase the number
           }
         }
       }
-      board[r][c].adjacentMines = count // Set the count of adjacent mines
+      board[r][c].adjacentMines = count // Set the count for each cell
     }
   }
 }
 
-// Function to create a unique ID for each cell
+//5) Function to give a unique ID for each cell
 const squareId = (r, c) => {
   return `cell(${r},${c})`
-}
+} //results like cell(0,0)
 
-// Function to print the grid on the screen
-const printGrid = () => {
+//6) Function to print the result of the board function on the screen
+const renderGrid = () => {
   let table, tr, td
-  const grid = document.querySelector('#grid')
   grid.textContent = '' // Clear previous grid
   table = document.createElement('table')
   grid.appendChild(table)
@@ -120,69 +174,96 @@ const printGrid = () => {
 
     for (let c = 0; c < board[r].length; c++) {
       td = document.createElement('td')
-      td.id = squareId(r, c)
+      td.id = squareId(r, c) //give id to the cell
       td.addEventListener('click', () => {
+        //add event listener to all grid cells
         if (!gameStarted) {
+          //if the player click on a cell the game, and starts
           startTimer()
           gameStarted = true
         }
-        revealCell(r, c)
+        revealCell(r, c) //when the user click reveal the value of the cell
       })
+
+      //contextmenu: An element is right-clicked to open a context menu, from w3school "HTML DOM Events"
       td.addEventListener('contextmenu', (e) => {
-        e.preventDefault()
-        flagCell(r, c)
+        e.preventDefault() //prevent the default menu
+        flagCell(r, c) //add a flag emoji when the player right click
       })
-      td.textContent = ''
+      td.textContent = '' //all the cells have the value of empty string
       tr.appendChild(td)
     }
   }
 }
+
+//7)a function that return true when gameOver
 const gameOver = () => {
-  // Iterate over the board and reveal all mines
   for (let r = 0; r < board.length; r++) {
     for (let c = 0; c < board[r].length; c++) {
       if (board[r][c].mine) {
+        //if the cell contain mine, show the emoji
         const td = document.getElementById(squareId(r, c))
-        td.classList.add('mine') // Add mine class for styling
+        td.classList.add('mine')
         td.innerHTML = emojis.mine // Show the mine emoji
       }
     }
   }
   clearInterval(timer) // Stop the timer
-  messageBox.textContent = 'Game Over! You hit a Shark!' // Show game over message
+  emojiButton.innerHTML = emojis.lose //change the emoji button value
+  messageBox.textContent = 'Game Over! You hit a Shark!' //GameOver Message
+  body.style.backgroundImage = images.underwater.lose //gameOver background
+  isGameOver = true
+  loseAudio.play()
 }
-// Function to reveal the cell when the player clicks
+//8) a Function that reveals the cell when the player clicks
 const revealCell = (row, column) => {
-  // If the cell was revealed before, or flagged, then return
-  if (board[row][column].revealed || board[row][column].flagged) {
+  // return when the player win, or lose, or the cell is revealed or flagged before
+  if (
+    board[row][column].revealed ||
+    board[row][column].flagged ||
+    isGameOver ||
+    isWin
+  ) {
     return
   }
-
+  //else
+  const td = document.getElementById(squareId(row, column))
+  td.classList.add('revealed') //add a class to the cell
   board[row][column].revealed = true
   revealedCount++
-
-  const td = document.getElementById(squareId(row, column))
-  td.classList.add('revealed')
 
   // Check if the cell is a mine
   if (board[row][column].mine) {
     gameOver()
-
-    body.style.backgroundImage =
-      "url('https://th.bing.com/th/id/R.75da500dcfd5b8dcd13d811667205f55?rik=0euedb0IWDkLOw&riu=http%3a%2f%2fmedia.giphy.com%2fmedia%2fNZymcDPuVrVZu%2fgiphy.gif&ehk=8xus2w%2f5xowTgSx2aDren6WW4fTh1EGFJHXsmBpMITY%3d&risl=&pid=ImgRaw&r=0')"
-
-    audio.play()
     return
   }
-  // Show the number of adjacent mines or an empty string if there are none
+  // Show the number of adjacent mines
   if (board[row][column].adjacentMines > 0) {
     td.textContent = board[row][column].adjacentMines // Show the count of adjacent mines
+
+    //give each number its unique color
+    if (td.textContent === '1') {
+      td.style.color = 'blue'
+    } else if (td.textContent === '2') {
+      td.style.color = 'green'
+    } else if (td.textContent === '3') {
+      td.style.color = 'red'
+    } else if (td.textContent === '4') {
+      td.style.color = 'darkblue'
+    } else if (td.textContent === '5') {
+      td.style.color = '#480303'
+    } else if (td.textContent === '6') {
+      td.style.color = '#009ba0'
+    } else if (td.textContent === '7') {
+      td.style.color = 'black'
+    } else if (td.textContent === '8') {
+      td.style.color = 'gray'
+    }
   } else {
     td.textContent = '' // Show nothing if there are no adjacent mines
-    td.style.backgroundColor = 'grey'
-    td.style.border = 'none'
   }
-
+  td.style.backgroundColor = '#aca9bb'
+  td.style.border = 'none'
   // If there are no adjacent mines, reveal adjacent cells
   if (board[row][column].adjacentMines === 0) {
     for (let i = -1; i <= 1; i++) {
@@ -191,41 +272,51 @@ const revealCell = (row, column) => {
         const newCol = column + j
         // Check if the new cell is within the board boundaries
         if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < columns) {
-          revealCell(newRow, newCol) // Recursively reveal adjacent cells
+          revealCell(newRow, newCol)
         }
       }
     }
   }
-  checkWin()
+  checkWin() //checkWin after each cell reveal
 }
 
-// Function to put a flag if the cell was right-clicked
+//9) Function to put a flag if the cell was right-clicked
 const flagCell = (row, column) => {
   const td = document.getElementById(squareId(row, column))
   const cell = board[row][column]
 
-  cell.flagged = !cell.flagged // Toggle the flagged state
+  if (cell.revealed || isGameOver || isWin) {
+    return
+  }
 
+  cell.flagged = !cell.flagged // Toggle the flagged cell state
   td.classList.toggle('flag')
 
   if (cell.flagged) {
-    flagClick.play()
+    flagClick.play() //start the sound effect
     td.textContent = emojis.flag // Show flag emoji
+    minesRemain.innerHTML--
   } else {
     td.textContent = '' // Clear the cell
+    emojiButton.textContent = emojis.normal //change the emoji back to normal
+    minesRemain.innerHTML++
   }
 }
 
-// Function to check winning condition
+//10) Function to check if the player win
 const checkWin = () => {
   if (revealedCount === totalCells) {
+    //if all the safe cells revealed
+    isWin = true
+    winAudio.play()
     messageBox.textContent = 'Congratulations, You Win!'
-    clearInterval(timer)
+    clearInterval(timer) //stop the stopwatch
+    emojiButton.innerHTML = emojis.win
+    body.style.backgroundImage = images.underwater.win
   }
 }
-body = document.getElementById('index')
-messageBox = document.getElementById('message')
-// Function to restart the game
+
+//11) Function to restart the game
 const restartGame = () => {
   clearInterval(timer)
   seconds = 0
@@ -233,39 +324,78 @@ const restartGame = () => {
   revealedCount = 0
   gameStarted = false
   timer = false
+  isWin = false
   totalCells = 0
   initiateBoard()
-  printGrid()
-  body.style.backgroundImage =
-    "url('https://th.bing.com/th/id/R.a61d96f23882c8e5a47ab16a6a7d7207?rik=QGLtMMZ1AoMqYw&riu=http%3a%2f%2fwww.pixelstalk.net%2fwp-content%2fuploads%2f2016%2f06%2fOcean-underwater-light-wallpaper-hd.jpg&ehk=RLmSM8K8ZQQn3YDnpMQGZfb5E3dJHQENNRqudKJONlY%3d&risl=1&pid=ImgRaw&r=0')"
+  renderGrid()
+  isGameOver = false
+  body.style.backgroundImage = images.underwater.normal
 
+  emojiButton.textContent = emojis.normal
   messageBox.textContent = ''
   grid.style.fontSize = '1em'
-  audio.pause()
+  loseAudio.pause()
+  minesRemain.textContent = mines
 }
 
-// Setup event listeners for difficulty buttons
+//  Event listeners
+//for reset button
 document.getElementById('reset-button').addEventListener('click', restartGame)
 
+//For difficulty buttons
 beginner.addEventListener('click', () => {
   rows = 9
   columns = 9
   mines = 10
   restartGame()
 })
-
 intermediate.addEventListener('click', () => {
   rows = 16
   columns = 16
   mines = 40
   restartGame()
 })
-
 advance.addEventListener('click', () => {
   rows = 16
   columns = 30
   mines = 99
   restartGame()
 })
+
+// const underwater = addEventListener('')
+const title = document.getElementById('title')
+const navigation = document.getElementById('navigation')
+original.addEventListener('click', () => {
+  gameMode.textContent = 'ORIGINAL'
+  gameDisplayers.forEach((displayer) => {
+    console.log(displayer)
+    displayer.style.backgroundColor = 'white'
+    displayer.style.color = '#13354b'
+  })
+  body.style.backgroundImage = images.original.normal
+  title.style.color = '#d4ede9'
+  grid.style.borderColor = '#3a4856'
+  navigation.style.color = 'white'
+  minesRemain.style.color = '#13354b'
+  emojis.mine = gameMine.original
+  minesRemain.style.backgroundColor = 'white'
+})
+
+underwater.addEventListener('click', () => {
+  gameMode.textContent = 'UNDERWATER'
+  gameDisplayers.forEach((displayer) => {
+    console.log(displayer)
+    displayer.style.backgroundColor = 'white'
+    displayer.style.color = '#13354b'
+  })
+  body.style.backgroundImage = images.original.normal
+  title.style.color = ''
+  grid.style.borderColor = ''
+  navigation.style.color = ''
+  minesRemain.style.color = ''
+  emojis.mine = gameMine.original
+  minesRemain.style.backgroundColor = ''
+})
+
 // Start the game for the first time
 restartGame()
